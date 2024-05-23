2767f17bb0a7a3efd64d25910339d64a90c03d90
jafarbdurr <jafarbdurr@gmail.com>
2024-05-23 19:49:26 +0700

    profile update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = 3000
 
 app.set(`view engine`, `ejs`)
 app.use(express.urlencoded ({extended:true}))
+app.use(express.static(__dirname + '/public'));
 
 app.use(session({
     secret: 'IntelliTrade',
